622ba1c819e9f1dccd7628fcf9e37475cfcd0166
Chris Andrews <chris@nodnol.org>
2012-11-02 18:29:18 +0000

    Use node indicated by npm, and fall back to 0.6.x process.arch.
    We were always using node from the path, rather than respecting the
    $npm_config_prefix variable.
    
    We also used the 'process.config' object, which isn't available until
    node v0.8.x, so we now fall back on process.arch in that case.

--- libusdt-build.sh
+++ libusdt-build.sh
@@ -4,13 +4,23 @@
 #
 unset MAKEFLAGS
 
+# Attempt to use the appropriate node binary - respect npm if that's
+# how we're being built, otherwise use node from PATH
+if [ -z $npm_config_prefix ]; then
+  NODE=`which node`
+else
+  NODE="$npm_config_prefix/bin/node"
+fi
+echo "Using node: $NODE"
+
 # Ask node what arch it's been built for, and build libusdt to match.
 #
 # (this will need to change at the point that GYP is able to build
 # node extensions universal on the Mac - for now we'll go with x86_64
-# on a 64 bit Mac)
+# on a 64 bit Mac, because that's the default architecture in that
+# situation).
 #
-ARCH=`node -e "console.log(process.config.variables.target_arch == 'x64' ? 'x86_64' : 'i386')"`
+ARCH=`$NODE libusdt-arch.js`
 echo "Building libusdt for ${ARCH}"
 export ARCH
 
